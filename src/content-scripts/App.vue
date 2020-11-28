b9f2db2e78e457d485984a94da41cf1a0696903d
plusye <yejiafe@gmail.com>
2020-11-28 22:55:16 +0800

    知乎，简书，腾讯文档

---
+++ src/content-scripts/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  methods: {
+    isZhihu() {
+      if (document.domain === 'link.zhihu.com') {
+        console.log(document.URL.indexOf('target='));
+        const url = document.URL.slice(document.URL.indexOf('target=') + 7);
+        console.log(decodeURIComponent(url));
+        window.location.href = decodeURIComponent(url);
+      }
+    },
+
+    isJianshu() {
+      if (document.URL.indexOf('jianshu.com') > 0 && document.URL.indexOf('go-wild') > 0) {
+        console.log(document.URL.indexOf('url='));
+        const url = document.URL.slice(document.URL.indexOf('url=') + 4);
+        console.log(decodeURIComponent(url));
+        window.location.href = decodeURIComponent(url);
+      }
+    },
+
+    isQQDocs() {
+      if (document.URL.indexOf('docs.qq.com') > 0 && document.URL.indexOf('scenario/link.html') > 0) {
+        console.log(document.URL.indexOf('url='));
+        const url = document.URL.slice(document.URL.indexOf('url=') + 4);
+        console.log(decodeURIComponent(url));
+        window.location.href = decodeURIComponent(url);
+      }
+    },
+  },
+  created() {
+    // console.log('yejia', document.URL, document.domain);
+    this.isZhihu();
+    this.isJianshu();
+    this.isQQDocs();
+  },
+  show() {
+  },
+};
+</script>
+
+<style>
+</style>
--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,1 +1,11 @@
-console.log('Hello from the content-script')
+import Vue from 'vue';
+import App from './App.vue';
+
+const elem = document.createElement('div');
+document.body.appendChild(elem);
+
+/* eslint-disable no-new */
+new Vue({
+  el: elem,
+  render: (h) => h(App),
+});
