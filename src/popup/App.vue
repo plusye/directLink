ae00308063b24a7084fe5e975dd067d6ca397165
plusye <yejiafe@gmail.com>
2020-11-28 23:40:26 +0800

    add popup page

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,19 +1,48 @@
 <template>
-  <hello-world />
+<div class="box">
+  <div>遇到不让直接跳转到第三方网址的网站？可以提交给我们。</div>
+  <div class="goTo" @click="submit">去提交</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: { HelloWorld }
-}
+  components: { },
+  methods: {
+    submit() {
+      window.open('https://github.com/plusye/directLink/issues');
+    },
+  },
+};
 </script>
 
 <style>
 html {
-  width: 400px;
-  height: 400px;
+  width: 200px;
+  height: 110px;
+  font-size: 18px;
+  padding: 5px;
+}
+
+.box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.goTo {
+  font-size: 20rpx;
+  background: #1CC287;
+  color: #ffffff;
+  padding: 8px;
+  width: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px;
 }
 </style>
